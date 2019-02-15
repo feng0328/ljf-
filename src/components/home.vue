5a1790c06a0b573fc4c8025cdde8cc365d8e4d98
ljf <296348573@qq.com>
2019-02-15 23:01:28 +0800

    home-布局组件引入

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>
-        home----
-    </div>
+  <el-container class="container">
+    <el-header class="middle">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
-}
+    data() {
+        return {
+            
+        }
+    },
+};
 </script>
 
 <style>
+.container {
+  height: 100%;
 
+  background: #b3c0d1;
+}
+.middle {
+  text-align: center;
+  line-height: 60px;
+}
+.aside {
+  background: red;
+}
+.main {
+  background: blue;
+}
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
